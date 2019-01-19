8653d22e99383920f5ef34bc19b20f508fd89011
sr-mobileappdev <coltonanglin119@gmail.com>
2019-01-18 17:48:21 -0800

    New APP

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+  apiKey = '386ba8f72d75a6fcfc022a9f66524fc6';
+  url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url + '/' + state + '/' + city + '.json')
+    .map(res => res.json());
+
+  }
+
+}
